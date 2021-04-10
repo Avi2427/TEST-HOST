508a2ec879af01d6fae642ffa8dd0219a1a3b8ee
Avinash <you@example.com>
2021-04-10 15:50:53 +0530

    FIRST

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log('Iam Online Now!!!')
+})
+
+bot.on('message', (message) => {
+    if(message.author.bot) return;
+    if(message.channel.type !== 'text') return;
+    let prefix = '-';
+    let MessageArray = message.content.split(' ');
+    let cmd = MessageArray[0]
+    let args = MessageArray.slice(1)
+    if(!message.content.startsWith(prefix)) return;
+
+    if(cmd == 'hello') {
+        message.channel.send('Hello Human');
+    }
+})
+
+bot.login(process.env.token)
